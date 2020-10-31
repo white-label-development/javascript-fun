6fc1ea8094b84be173d19c25b6609ebc59fd9267
Neil Thompson <neil@white-label-development.co.uk>
2020-10-31 13:03:59 +0000

    continue code along (abandoned) stopped at 6.9

--- Vue/VueJsFundamentals/dev/build-a-bot/src/App.vue
+++ Vue/VueJsFundamentals/dev/build-a-bot/src/App.vue
@@ -12,6 +12,9 @@
           <li class="nav-item">
             <router-link class="nav-link" :to="{ name: 'Build' }" exact>Build</router-link>
           </li>
+          <li class="nav-item cart">
+            <router-link class="nav-link" :to="{ name: 'Cart' }" exact>Shopping Cart</router-link>
+          </li>
         </ul>
       </nav>
     </header>
